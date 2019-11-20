caa281f3125b1d059d68c885c37503efb42c5809
Alexander Rubenov <a.rubenov@tinkoff.ru>
2019-11-20 18:46:24 +0300

    passed 12 lecture

--- src/components/Article.js
+++ src/components/Article.js
@@ -5,7 +5,6 @@ class Article extends PureComponent {
         super(props)
 
         this.state = {
-            isOpen: props.defaultOpen,
             count: 0
         }
     }
@@ -20,27 +19,29 @@ class Article extends PureComponent {
         console.log('mounting')
     }
 
+    /*
     componentWillReceiveProps(nextProps) {
         if (nextProps.defaultOpen !== this.props.defaultOpen) this.setState({
             isOpen: nextProps.defaultOpen
         })
     }
+    */
 
     componentWillUpdate() {
         console.log('will update')
     }
 
     render() {
-        const { article } = this.props
-        const body = this.state.isOpen && <section className="card-text">{article.text}</section>
+        const { article, isOpen, onButtonClick } = this.props
+        const body = isOpen && <section className="card-text">{article.text}</section>
         return (
             <div className="card mx-auto" style={{width: '50%'}}>
                 <div className="card-header">
                     <h2 onClick={this.incrementCounter}>
                         {article.title}
                         clicked {this.state.count}
-                        <button className="btn btn-primary btn-lg float-right" onClick={this.handleClick}>
-                            {this.state.isOpen ? 'close' : 'open'}
+                        <button className="btn btn-primary btn-lg float-right" onClick={onButtonClick}>
+                            {isOpen ? 'close' : 'open'}
                         </button>
                     </h2>
                 </div>
@@ -57,12 +58,6 @@ class Article extends PureComponent {
     incrementCounter = () => {
         this.setState({
             count: this.state.count + 1
-        })
-    }
-
-    handleClick = () => {
-        this.setState({
-            isOpen: !this.state.isOpen
         })
     }
 }
--- src/components/ArticlesList/index.js
+++ src/components/ArticlesList/index.js
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import Article from '../Article'
 import './style.css'
 
-export default function ArticlesList({ articles }) {
-    const articleElements = articles.map((article, index) => 
-        <li className="article-list__li" key={article.id}>
-            <Article article={article} defaultOpen={index === 0} />
-        </li>
-    )
-    return (
-        <ul>
-            {articleElements}
-        </ul>
-    )
+export default class ArticlesList extends PureComponent {
+    render() {
+        const articleElements = this.props.articles.map((article, index) =>
+            <li className="article-list__li" key={article.id}>
+                <Article article={article} defaultOpen={index === 0} />
+            </li>
+        )
+        return (
+            <ul>
+                {articleElements}
+            </ul>
+        )
+    }
 }
